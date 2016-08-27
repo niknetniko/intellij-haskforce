29bc8ead94ea3b44b6973e09bd78b4036796096b
Cary M. Robbins <carymrobbins@gmail.com>
2016-08-26 21:14:33 -0400

    refs #294: Avoid NPE by using Option

--- src/com/haskforce/cabal/lang/psi/impl/ModulePartImpl.scala
+++ src/com/haskforce/cabal/lang/psi/impl/ModulePartImpl.scala
@@ -53,27 +53,29 @@ trait ModulePartImpl extends CabalNamedElementImpl {
     }.toArray[AnyRef]
   }
 
-  override def resolve(): PsiElement = {
+  override def resolve(): PsiElement = resolveOpt().orNull
+
+  private def resolveOpt(): Option[PsiElement] = {
     val lastPart = getContext.getLastPart
     if (this != lastPart) resolveToModuleDir(lastPart) else resolveToModuleDecl()
   }
 
   private val DOT_REGEX = Pattern.compile("\\.")
 
-  private def resolveToModuleDir(lastPart: ModulePart): PsiDirectory = {
+  private def resolveToModuleDir(lastPart: ModulePart): Option[PsiDirectory] = {
     // Find the part's position from the end so we can walk up the directory tree.
     val revPos = getParent.getChildren.reverse.indexOf(this)
     if (revPos == -1) throw new AssertionError(s"$getText not in parent (${getParent.getText})")
     // Iterate up the directory tree 'revPos' times.
     Stream.iterate(
-      lastPart.resolve().getContainingFile.getContainingDirectory, revPos
-    )(_.getParent).lastOption.filter(
+      lastPart.resolveOpt().map(_.getContainingFile.getContainingDirectory), revPos
+    )(_.map(_.getParent)).flatten.lastOption.filter(
       // Ensure that the found directory name matches our element.
       dir => dir.getName == getText
-    ).orNull
+    )
   }
 
-  private def resolveToModuleDecl(): PsiElement = {
+  private def resolveToModuleDecl(): Option[PsiElement] = {
     // If the module part IS the last part, resolve to its file's module decl.
     val qualifiedName = getParent.getText
     val scope = Option(ModuleUtilCore.findModuleForPsiElement(this)) match {
@@ -92,6 +94,6 @@ trait ModulePartImpl extends CabalNamedElementImpl {
     }.orElse {
       // If there are no matches, just guess at the first file found, if exists.
       if (files.size == 0) None else Option(files.get(0))
-    }.orNull
+    }
   }
 }
